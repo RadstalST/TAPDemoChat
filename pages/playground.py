525fb1589ae24d1675053014282221f2c9f920ae
Suchat Tangjarukij <suchattangjarukij@Suchats-MacBook-Air.local>
2023-10-11 11:03:20 +1100

    development

--- pages/playground.py
+++ pages/playground.py
@@ -16,53 +16,52 @@ prompt_template = PromptTemplate.from_template(
                     '''"""
                 ) 
 generated_prompt = ""
-def show_question_pane():
-    with questionPane:
-        final_prompt = ""
-        with st.container() as form:
-            option = st.selectbox("bot option",('GPT4', 'GPT4+ToT', 'GPT4+CoT',"GPT+CoT+Chroma"))
-            with st.expander("description"):
-                st.write("This is a description")
-        
-            col1, col2 = st.columns(2)
-            with col1: 
-                st.subheader("User Input")
-                userInput = st.text_area(
-                    "Your input goes here:", 
-                    placeholder="I have problem with headache today. I worked 10 hours yesterday",
-                    key='input',
-                    height=400)
-            with col2:
-                st.subheader("Scenario")
-                prompt = st.text_area(
-                    "Your prompt goes here:", 
-                    key='prompt', height=400, 
-                    value="Please provide possible symptom with my problem")
-                
 
-            if prompt:
-                final_prompt = prompt
-            else:
-                final_prompt = option
-            with st.expander("See Generated Prompt"):
-                
-                generated_prompt = prompt_template.format(userInput=userInput, prompt=final_prompt)
-              
-                # st.write(form.__dict__)
-                
-
-
+with questionPane:
+    final_prompt = ""
+    with st.container() as form:
+        option = st.selectbox("bot option",('GPT4', 'GPT4+ToT', 'GPT4+CoT',"GPT+CoT+Chroma"))
+        with st.expander("description"):
+            st.write("This is a description")
+    
+        col1, col2 = st.columns(2)
+        with col1: 
+            st.subheader("User Input")
+            userInput = st.text_area(
+                "Your input goes here:", 
+                placeholder="I have problem with headache today. I worked 10 hours yesterday",
+                key='input',
+                height=300)
+        with col2:
+            st.subheader("Scenario")
+            prompt = st.text_area(
+                "Your prompt goes here:", 
+                key='prompt', height=300, 
+                value="Please provide possible symptom with my problem")
             
 
-        with st.form("playground_form") as form:
-            st.markdown(generated_prompt)
+        if prompt:
+            final_prompt = prompt
+        else:
+            final_prompt = option
+        with st.expander("See Generated Prompt"):
+            
+            generated_prompt = prompt_template.format(userInput=userInput, prompt=final_prompt)
+            
+            # st.write(form.__dict__)
+            
 
-            submit_button = st.form_submit_button(label='Send')
-            if submit_button:
-                st.write(userInput)
-                st.write(prompt)
 
-        feedback = st.text_area("your feedback:", key='prompt_output', height=50)
+        
+st.divider()
+with st.form("playground_form") as form:
+    st.markdown(generated_prompt)
+
+    submit_button = st.form_submit_button(label='Send')
+    if submit_button:
+        st.write(userInput)
+        st.write(prompt)
+
+feedback = st.text_area("your feedback:", key='prompt_output', height=50)
 
 
-show_question_pane()
